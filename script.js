3cf99291127d7dfe72c03d05ff3f2370a82dffae
Alexey <alexeynayko@ukr.net>
2026-02-21 23:25:13 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+//alert('Hello');
+//const result = confirm("Are you here?");
+//console.log(result);
+///alert(result);
+//const answer = +prompt("Are you 18 years old?", "18");
+/* const answer = +prompt("Are you 18 years old?", "18"); */
+//console.log(typeof(answer));
+/* console.log(answer+5); */
+/* const answers =[];
+
+answers[0] =prompt ('Whats your First name', '');
+answers[1] =prompt ('Whats your Last name', '');
+answers[2] =prompt ('How old are you?', '')
+//document.write(answers);
+
+
+console.log(typeof(answers));
+console.log(typeof(null)); */
+
+console.log('arr' + "-object");
+console.log(4+ +"5");
+
+let incr=10,
+    decr=10;
+
+/* ++incr;
+--decr;
+ */
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2+2*2 !== '6');
+
+&& 
+||
+
+const isChecked = false,
+      isClosed = false;
+
+console.log (isChecked || !isClosed);
