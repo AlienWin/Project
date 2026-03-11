388a5b4e529865e3570a2ddfbcd9b26b3cbac8d0
Alexey <alexeynayko@ukr.net>
2026-03-11 11:17:03 +0200

    latest changes

---
+++ lab217.js
@@ -0,0 +1,25 @@
+"user strict";
+
+let num =50;
+
+//while (num<=55) {
+ //   console.log(num);
+   // num ++
+//}
+//do {
+  //  console.log(num);
+    //numm ++;
+//}while (num <55);
+
+/* for (let i =1; i<9; i++) {
+    console.log(num);
+    num++;
+} */
+for (let i =1; i<10; i++) {
+    if (i === 6) {
+       // break;
+       continue;
+    }
+    console.log(i);
+  
+}
